50e54853324b35b40cfd359010e577f7298bd0ef
leehyeeun <leehe0430@gmail.com>
2023-09-23 21:56:08 +0900

    elasticsearch8 init

---
+++ elasticsearch8/src/main/java/lee/elasticsearch8/Elasticsearch8Application.java
@@ -0,0 +1,13 @@
+package lee.elasticsearch8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elasticsearch8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Elasticsearch8Application.class, args);
+	}
+
+}
