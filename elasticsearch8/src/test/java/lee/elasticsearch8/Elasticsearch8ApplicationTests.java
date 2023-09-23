b8f0efa2dc184a24923e67eb08690198f43e3d47
leehyeeun <leehe0430@gmail.com>
2023-09-23 21:54:47 +0900

    elasticsearch8 gitignore init

---
+++ elasticsearch8/src/test/java/lee/elasticsearch8/Elasticsearch8ApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.elasticsearch8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Elasticsearch8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
