3888db6646911b26fbaefcd89736beb1cec5e538
leehyeeun <leehe0430@gmail.com>
2023-12-03 18:18:35 +0900

    ModernJavaInAction study init

---
+++ ModernJavaInAction/src/test/java/book/java/ModernJavaInAction/ModernJavaInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package book.java.ModernJavaInAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
