b5b63c07d3f829a5794acaf8f10df8a2c5f9ae99
leehyeeun <leehe0430@gmail.com>
2024-03-04 21:10:58 +0900

    advanced spring boot init

---
+++ advanced-springboot/src/main/java/lee/project/advancedspringboot/AdvancedSpringbootApplication.java
@@ -0,0 +1,13 @@
+package lee.project.advancedspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedSpringbootApplication.class, args);
+	}
+
+}
---
+++ advanced-springboot/src/test/java/lee/project/advancedspringboot/AdvancedSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.project.advancedspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
